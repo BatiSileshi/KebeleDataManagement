ad4c434e0804bd23fff6beb427e6ca00fa5a822e
Barnaan <barnaantekalign@gmail.com>
2023-02-10 02:55:43 +0300

    some change

--- static/vendors/local_bmgt.js
+++ static/vendors/local_bmgt.js
@@ -2,7 +2,7 @@
 const businesName = document.getElementById("id_name");
 businesName.addEventListener('keypress',(event)=>{
       let inputedValue = event.charCode;
-      if(!(inputedValue>=65&&inputedValue<=122) && (inputedValue =32 && inputedValue !=0)){
+      if(!(inputedValue>=65&&inputedValue<=122) && (inputedValue!=32 && inputedValue !=0)){
           event.preventDefault() 
       }
 });
