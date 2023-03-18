f02edfdc0cb79bdf34af4f3840a25f4db4147926
Bati Sileshi <batisileshi93@gmail.com>
2023-03-18 02:15:26 -0400

    Update settings.py

--- KebeleDataManagement/settings.py
+++ KebeleDataManagement/settings.py
@@ -134,12 +134,6 @@ USE_TZ = True
 
 # Sending mail
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST ='smtp.gmail.com'
-EMAIL_PORT = 587 
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mantinakebele@gmail.com'
-EMAIL_HOST_PASSWORD = 'oncirxwqnfsflyvl'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
